1612482740ac8e943e111c2efb1f5d16b6350155
Bùi Minh Quân <bmq1603@gmail.com>
2026-07-12 23:36:15 +0700

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.java
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder newStr= new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                newStr.append(Character.toLowerCase(c));
+        int l = 0;
+        int r = s.length() - 1;
+        
+        while (l < r) {
+            // Nếu con trỏ bên trái không phải là chữ/số, bỏ qua và tiến tới
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
             }
+            // Nếu con trỏ bên phải không phải là chữ/số, bỏ qua và lùi lại
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            
+            // So sánh hai ký tự (chuyển về chữ thường để không phân biệt hoa thường)
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            
+            l++;
+            r--;
         }
-        return newStr.toString().equals(newStr.reverse().toString());
+        
+        return true;
     }
 }
